0b8549eda054c8a983a732c4d2aea959eef3989a
DevutMX <devutmx@gmail.com>
2017-10-20 01:34:14 -0500

    Creacion del dashboard

---
+++ WebShop/Backend.Master.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebShop {
+    
+    
+    public partial class Backend {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control HeaderMenu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxMenu HeaderMenu;
+        
+        /// <summary>
+        /// Control MainContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+    }
+}
---
+++ WebShop/Dashboard.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Backend.Master" CodeBehind="Dashboard.aspx.cs" Inherits="WebShop.Dashboard" %>
+
+<asp:Content ID="Content" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <table width="100%">
+            <tr>
+                <th colspan="3"><div align="center">Opciones disponibles</div></th>
+            </tr>
+            <tr>
+                <td width="33%"><div align="center"><a href="#"><img src="Images/sistema/Clientes.png" alt="Clientes"/><br />Clientes</a></div></td>
+                <td width="33%"><div align="center"><a href="#"><img src="Images/sistema/Ventas.png" alt="Ventas"/><br />Ventas</a></div></td>
+                <td width="33%"><div align="center"><a href="#"><img src="Images/sistema/Usuarios.png" alt="Usuarios"/><br />Usuarios</a></div></td>
+            </tr>
+            <tr>
+                <td width="33%"><div align="center"><a href="#"><img src="Images/sistema/Pago.png" alt="Formas de pago"/><br />Formas de pago</a></div></td>
+                <td width="33%"><div align="center"><a href="#"><img src="Images/sistema/Cerrar.png" alt="Cerrar Sesion"/><br />Cerrar Sesión</a></div></td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
